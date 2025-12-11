16cd1fe50f091ddd3f7ab8b5c3adfbf64397fc0f
Ponttunen <mymememail2018@protonmail.com>
2025-12-11 11:14:07 +0200

    Making projects src started, created the 2 first src files (nothing really inside them) and made a .pylintrc -file.

---
+++ src/app.py
@@ -0,0 +1,6 @@
+"""App-module for the functions of the program"""
+
+class App:
+    """App class, which hads the methods of the program."""
+    def __init__(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from app import App
+
+def main():
+    """Starts the program."""
+    app = App()
+    app.run()
+
+if __name__ == "__main__":
+    main()
